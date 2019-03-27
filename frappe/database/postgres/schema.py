af7de92797cd32a30116bb6e71816b09f7157e3f
kratos <sunil.goli@zerodha.com>
2019-03-27 18:10:44 +0530

    Bug-fix: Update alter query for smallest (Check) conversion

--- frappe/database/postgres/schema.py
+++ frappe/database/postgres/schema.py
@@ -48,6 +48,9 @@ class PostgresTable(DBTable):
 				# Add USING :: timestamp if type is datetime
 				using_clause = "USING {}::timestamp without time zone".format(col.fieldname)
 
+			elif col.fieldtype in ("Check"):	
+				using_clause = "USING {}::smallint".format(col.fieldname)
+
 			query.append("ALTER COLUMN `{}` TYPE {} {}".format(
 				col.fieldname, 
 				get_definition(col.fieldtype, precision=col.precision, length=col.length), using_clause))
